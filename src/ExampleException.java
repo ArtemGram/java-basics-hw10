1a4a12f0b8f16f09dd4321af7bddbe5a0bbf3a6f
ArtemGram <artem.gramushnyak@ringteam.com>
2020-04-05 19:40:30 +0300

    1.Add exception

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,5 +1,8 @@
 public class ExampleException {
   public static int rectangleArea(int a, int b) {
+    if (a<0 || b<0) {
+      throw new IllegalArgumentException("input value is below zero!");
+    }
     return a * b;
   }
 }
